2f44714903a1817290b43f98f8bccb438f22cde2
Chris Coffey <cmcof273@gmail.com>
2016-01-08 07:42:11 -0500

    parsing the binary correctly

--- code/SynacorVm.hs
+++ code/SynacorVm.hs
@@ -1,6 +1,27 @@
 module Main where
 
+import qualified Data.ByteString.Lazy as LB
+import Data.ByteString.Builder
+import Data.Binary.Get
+import Data.Word
+import Data.Monoid
+
+deserializeInst :: Get [Word16]
+deserializeInst = do
+    empty <- isEmpty
+    if empty
+        then return []
+        else do v <- getWord16le
+                rest <- deserializeInst
+                return (v:rest)
+
+joinWords :: [Word16] -> Builder
+joinWords [] = mempty
+joinWords (w:ws) =
+    (word16LE w) <> mconcat [word16LE w' | w' <- ws]
+
 main :: IO ()
 main = do 
-    contents <- getContents
-    putStr contents
+    contents <- LB.getContents
+    --print $ runGet (toLazyByteString . joinWords . deserializeInst) contents
+    print $ runGet deserializeInst contents
